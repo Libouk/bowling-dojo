d37d89219211b3face548b4c3a91d6ffb8f2ff0d
Quentin Lebouc <quentin.lebouc@pix.fr>
2021-11-29 11:56:24 +0100

    first function and valid test

--- test/bowling_test.js
+++ test/bowling_test.js
@@ -1,5 +1,22 @@
 const { expect } = require('chai');
 
-it('fails', function() {
-   expect(1).to.equal(2);
+function calculateScore(rolls) {
+   return 0;
+}
+
+// (When scoring “X” indicates a strike, “/” indicates a spare, “-” indicates a miss)
+// -- -- -- -- -- -- -- -- -- --
+// X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30 points = 300
+// 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90
+// 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
+
+it('should obtain a score of 0 for all missed throws', function() {
+   // given
+   const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+   // when
+   const score = calculateScore(rolls);
+
+   // then
+   expect(score).to.equal(0);
 });
